d6ac98e3f247362f2404a0845ab6e1a953a93ed0
saleh <saleh.informatik@gmail.com>
2020-11-04 17:04:28 +0100

    lesson 18

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,8 +7,43 @@ $(function(){
     $(".toggle-submenu").on('click',function(){
         $(this).toggleClass("fa-angle-right fa-angle-down");
         $(this).parent('a').next('.child-links').slideToggle();
+    });
+    // Open / colse full screen
+    $('.toggle-fullscreen').on('click',function(){
+        $(this).toggleClass('full-screen');
+        if($(this).hasClass('full-screen')){
+            // page is now full screen
+            closeFullscreen();
+        }else{
+            openFullscrren();
+        }
     })
+
 });
+
+var elem = document.documentElement;
+
+function openFullscrren(){
+    if(elem.requestFullscreen){
+        elem.requestFullscreen();
+    }else if(elem.mozRequestFullScreen){
+        elem.mozRequestFullScreen();
+    }else if(elem.webkitRequestFullscreen){
+        elem.webkitRequestFullscreen();
+    }else if(elem.msRequestFullscreen){
+        elem.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen(){
+    if(document.exitFullscreen){
+        document.exitFullscreen();
+    }else if(document.mozCancelFullScreen){
+        document.mozCancelFullScreen();
+    }else if(document.webkitExitFullscreen){
+        document.webkitExitFullscreen();
+    }
+}
 const mymap= "nfsdsdfsd";
 var s= "dflsdjfsdjf";
 const { set } = require("lodash");
